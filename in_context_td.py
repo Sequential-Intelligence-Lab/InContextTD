53cc1cc670bf5779716dd509ea3bb06c0932624a
Jiuqi Wang <leonardowang597@gmail.com>
2024-03-18 00:18:36 -0400

    make reward positive

--- in_context_td.py
+++ in_context_td.py
@@ -15,15 +15,15 @@ class TFLayer:
         self.M[-1, -1] = 0
         I = np.eye(d)
         O = np.zeros((d, d))
-        self.M1 = stack_four(I, -I, O, O)
-        self.M2 = stack_four(I, O, O, O)
+        self.M1 = stack_four(-I, I, O, O)
+        # self.M2 = stack_four(I, O, O, O)
         # self.C = I 
         self.C = np.random.randn(d, d) 
         self.B = stack_four(self.C.T, O, O, O)
-        self.A = self.M2.T @ self.B @ self.M1
+        self.A = self.B @ self.M1
         self.Q = np.zeros(self.P.shape)
         self.Q[:2*d, :2*d] = self.A
-        self.Q = - self.Q
+        # self.Q = - self.Q
     
     def forward(self, Z):
         next_Z = Z + 1.0 / self.n * self. P @ Z @ self.M @ Z.T @ self.Q @ Z
@@ -39,7 +39,7 @@ class Transformer:
         v = []
         for layer in self.layers:
             Z  = layer.forward(Z)
-            v.append(Z[-1, -1])
+            v.append(-Z[-1, -1])
         return v, Z
 
 class Prompt:
@@ -56,7 +56,7 @@ class Prompt:
         self.r = np.reshape(self.r, (1, -1))
     
     def z(self):
-        return np.concatenate([self.phi, self.phi_prime, -self.r], axis=0)
+        return np.concatenate([self.phi, self.phi_prime, self.r], axis=0)
     
     def td_update(self, w, C):
         u = 0
