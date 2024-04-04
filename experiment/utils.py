ffbc5715d5c32c391b0c36437bad7ad91f330be1
Jiuqi Wang <leonardowang597@gmail.com>
2024-04-04 15:16:21 -0400

    add MSVE and MSPBE

--- experiment/utils.py
+++ experiment/utils.py
@@ -1,5 +1,11 @@
+from typing import Tuple
+
+import numpy as np
+import scipy as sp
 import torch
+
 import experiment.model as model
+
 
 def stack_four(A: torch.Tensor, B: torch.Tensor,
                C: torch.Tensor, D: torch.Tensor):
@@ -50,14 +56,67 @@ def manual_weight_extraction(tf: model.LinearTransformer,
     return weight.reshape((d, 1))
 
 
+def compute_steady_dist(P: np.array) -> np.ndarray:
+    '''
+    P: transition probability matrix
+    '''
+
+    n = P.shape[0]
+    null_vec = sp.linalg.null_space(np.eye(n) - P.T)
+
+    return (null_vec / np.sum(null_vec)).flatten()
+
+
+def solve_msve(P: np.ndarray,
+               X: np.ndarray,
+               v: np.ndarray) -> Tuple[np.ndarray, int]:
+    '''
+    P: transition probability matrix
+    X: feature matrix
+    v: true value
+    returns weight minimizing MSVE and the corresponding MSVE
+    '''
+    steady_dist = compute_steady_dist(P)
+    D = np.diag(steady_dist)
+    w = np.linalg.inv(X.T @ D @ X) @ X.T @ D @ v
+    msve = (X @ w - v).T @ D @ (X @ w - v)
+    return w, msve.item()
+
+
+def solve_mspbe(P: np.ndarray,
+                X: np.ndarray,
+                r: np.ndarray,
+                gamma: float) -> Tuple[np.ndarray, int]:
+    '''
+    P: transition probability matrix
+    X: feature matrix
+    r: reward vector
+    gamma: discount factor
+    returns weight minimizing MSPBE and the corresponding MSPBE
+    '''
+    n = P.shape[0]
+    steady_dist = compute_steady_dist(P)
+    D = np.diag(steady_dist)
+
+    A = X.T @ D @ (gamma*P - np.eye(n)) @ X
+    b = - X.T @ D @ r
+    w = np.linalg.inv(A) @ b
+
+    projection = X @ np.linalg.inv(X.T @ D @ X) @ X.T @ D
+    v_hat = X @ w
+    pbe = projection @ (r + gamma * P @ v_hat) - v_hat
+    mspde = pbe.T @ D @ pbe # this quantity should be very close to zero because td fixed point brings MSPBE to zero
+
+    return w, mspde.item()
+
+
+
 if __name__ == '__main__':
-    from experiment.prompt import Prompt
-    from experiment.model import LinearTransformer
-    d = 3
-    n = 40
-    l = 5
-    pro = Prompt(d, n, 0.9)
-    Z_0 = pro.z()
-    tf = LinearTransformer(d, n, l, mode='auto')
-    weight = manual_weight_extraction(tf, Z_0, d)
-    print(weight)
+    from experiment.boyan import BoyanChain
+    bc = BoyanChain(10)
+    X = np.random.randn(10, 3)
+    w_msve, msve = solve_msve(bc.P, X, bc.v)
+    print(w_msve, msve)
+
+    w_mspbe, mspbe = solve_mspbe(bc.P, X, bc.r, bc.gamma)
+    print(w_mspbe, mspbe)
