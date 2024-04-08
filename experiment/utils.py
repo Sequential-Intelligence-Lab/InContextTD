54c62253368084eb472cfefcb3354a9e91ba72c8
Jiuqi Wang <leonardowang597@gmail.com>
2024-04-08 19:05:03 -0400

    optimize mspbe and msve computation

--- experiment/utils.py
+++ experiment/utils.py
@@ -50,7 +50,7 @@ def manual_weight_extraction(tf: model.LinearTransformer,
         query = torch.zeros((2*d+1, 1))
         query[i, 0] = -1
         Z_p = torch.concat([context, query], dim=1)
-        _, Z_tf = tf(Z_p)
+        Z_tf = tf(Z_p)
         weight.append(Z_tf[-1, -1])
     weight = torch.stack(weight, dim=0)
     return weight.reshape((d, 1))
@@ -79,7 +79,8 @@ def solve_msve(P: np.ndarray,
     steady_dist = compute_steady_dist(P)
     D = np.diag(steady_dist)
     w = np.linalg.inv(X.T @ D @ X) @ X.T @ D @ v
-    msve = np.sqrt((X @ w - v).T @ D @ (X @ w - v))
+    error = X @ w - v
+    msve = steady_dist.dot(error**2)
     return w, msve.item()
 
 
@@ -105,16 +106,17 @@ def solve_mspbe(P: np.ndarray,
     projection = X @ np.linalg.inv(X.T @ D @ X) @ X.T @ D
     v_hat = X @ w
     pbe = projection @ (r + gamma * P @ v_hat) - v_hat
-    mspde = np.sqrt(pbe.T @ D @ pbe) # this quantity should be very close to zero because td fixed point brings MSPBE to zero
+    mspde =  steady_dist.dot(pbe**2) # this quantity should be very close to zero because td fixed point brings MSPBE to zero
 
     return w, mspde.item()
 
 
 
 if __name__ == '__main__':
-    from experiment.boyan import BoyanChain
-    bc = BoyanChain(10)
+    from MRP.boyan import BoyanChain
+    np.random.seed(0)
     X = np.random.randn(10, 3)
+    bc = BoyanChain(n_states=10)
     w_msve, msve = solve_msve(bc.P, X, bc.v)
     print(w_msve, msve)
     w_mspbe, mspbe = solve_mspbe(bc.P, X, bc.r, bc.gamma)
