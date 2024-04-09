1b129af4d86eaf5ff4cb6e668a01bcb54c01bcb6
Jiuqi Wang <leonardowang597@gmail.com>
2024-04-09 14:39:34 -0400

    separate MSVE and MSPBE weight and value computation

--- experiment/utils.py
+++ experiment/utils.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 import numpy as np
 import scipy as sp
 import torch
@@ -67,49 +65,80 @@ def compute_steady_dist(P: np.array) -> np.ndarray:
     return (null_vec / np.sum(null_vec)).flatten()
 
 
-def solve_msve(P: np.ndarray,
-               X: np.ndarray,
-               v: np.ndarray) -> Tuple[np.ndarray, int]:
+def solve_msve_weight(steady_dist: np.ndarray,
+                      X: np.ndarray,
+                      v: np.ndarray) -> np.ndarray:
     '''
     P: transition probability matrix
     X: feature matrix
     v: true value
-    returns weight minimizing MSVE and the corresponding MSVE
+    returns weight minimizing MSVE
     '''
-    steady_dist = compute_steady_dist(P)
     D = np.diag(steady_dist)
-    w = np.linalg.inv(X.T @ D @ X) @ X.T @ D @ v
+    return np.linalg.inv(X.T @ D @ X) @ X.T @ D @ v
+
+
+def compute_msve(w: np.ndarray,
+                 steady_dist: np.ndarray,
+                 X: np.ndarray,
+                 v: np.ndarray) -> float:
+    '''
+    w: weight vector
+    steady_dist: steady state distribution
+    X: feature matrix
+    v: true value
+    returns MSVE
+    '''
     error = X @ w - v
     msve = steady_dist.dot(error**2)
-    return w, msve.item()
+    return msve.item()
 
 
-def solve_mspbe(P: np.ndarray,
-                X: np.ndarray,
-                r: np.ndarray,
-                gamma: float) -> Tuple[np.ndarray, int]:
+def solve_mspbe_weight(steady_dist: np.ndarray,
+                       P: np.ndarray,
+                       X: np.ndarray,
+                       r: np.ndarray,
+                       gamma: float) -> np.ndarray:
     '''
+    steady_dist: steady state distribution
     P: transition probability matrix
     X: feature matrix
     r: reward vector
     gamma: discount factor
-    returns weight minimizing MSPBE and the corresponding MSPBE
+    returns weight minimizing MSPBE
     '''
     n = P.shape[0]
-    steady_dist = compute_steady_dist(P)
     D = np.diag(steady_dist)
 
     A = X.T @ D @ (gamma*P - np.eye(n)) @ X
     b = - X.T @ D @ r
     w = np.linalg.inv(A) @ b
 
+    return w
+
+
+def compute_MSPBE(w: np.ndarray,
+                  steady_dist: np.ndarray,
+                  P: np.ndarray,
+                  X: np.ndarray,
+                  r: np.ndarray,
+                  gamma: float):
+    '''
+    w: weight vector
+    steady_dist: steady state distribution
+    P: transition probability matrix
+    X: feature matrix
+    r: reward vector
+    gamma: discount factor
+    returns MSPBE
+    '''
+    D = np.diag(steady_dist)
     projection = X @ np.linalg.inv(X.T @ D @ X) @ X.T @ D
     v_hat = X @ w
     pbe = projection @ (r + gamma * P @ v_hat) - v_hat
-    mspde =  steady_dist.dot(pbe**2) # this quantity should be very close to zero because td fixed point brings MSPBE to zero
-
-    return w, mspde.item()
-
+    # this quantity should be very close to zero because td fixed point brings MSPBE to zero
+    mspbe = steady_dist.dot(pbe**2)
+    return mspbe.item()
 
 
 if __name__ == '__main__':
@@ -117,7 +146,11 @@ if __name__ == '__main__':
     np.random.seed(0)
     X = np.random.randn(10, 3)
     bc = BoyanChain(n_states=10)
-    w_msve, msve = solve_msve(bc.P, X, bc.v)
-    print(w_msve, msve)
-    w_mspbe, mspbe = solve_mspbe(bc.P, X, bc.r, bc.gamma)
-    print(w_mspbe, mspbe)
+    w_msve = solve_msve_weight(bc.steady_d, X, bc.v)
+    print('MSVE Weight\n', w_msve)
+    msve = compute_msve(w_msve, bc.steady_d, X, bc.v)
+    print('MSVE\n', msve)
+    w_mspbe = solve_mspbe_weight(bc.steady_d, bc.P, X, bc.r, bc.gamma)
+    print('MSPBE Weight\n', w_mspbe)
+    mspbe = compute_MSPBE(w_mspbe, bc.steady_d, bc.P, X, bc.r, bc.gamma)
+    print('MSPBE\n', mspbe)
