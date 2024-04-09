6c87e70b6c5a8403dad9f4c63f9e0a350d30d454
Jiuqi Wang <leonardowang597@gmail.com>
2024-04-09 18:22:49 -0400

    support dynamic masking initialize weights according to depth

--- experiment/model.py
+++ experiment/model.py
@@ -11,21 +11,21 @@ class LinearAttention(nn.Module):
         super(LinearAttention, self).__init__()
         self.d = d
         self.n = n
+        self.lmbd = lmbd
 
         self.P = nn.Parameter(torch.empty(2 * d + 1, 2 * d + 1))
-        nn.init.xavier_normal_(self.P, gain=0.1)
-
-        self.M = torch.eye(n + 1)
-        for col in range(n):
-            for row in range(col+1, n):
-                self.M[row, col] = lmbd*self.M[row-1, col]
-        self.M[-1, -1] = 0
-
         self.Q = nn.Parameter(torch.empty(2 * d + 1, 2 * d + 1))
-        nn.init.xavier_normal_(self.Q, gain=0.1)
 
     def forward(self, Z):
-        return Z + 1.0 / self.n * self.P @ Z @ self.M @ Z.T @ self.Q @ Z
+        h = Z.shape[1]
+        # dynamic masking
+        M = torch.eye(h)
+        for col in range(self.n):
+            for row in range(col+1, self.n):
+                M[row, col] = self.lmbd*M[row-1, col]
+        for i in range(self.n, h):
+            M[i, i] = 0
+        return Z + 1.0 / self.n * self.P @ Z @ M @ Z.T @ self.Q @ Z
 
 
 class LinearTransformer(nn.Module):
@@ -46,10 +46,16 @@ class LinearTransformer(nn.Module):
         self.l = l
         self.mode = mode
         if mode == 'auto':
-            self.attn = LinearAttention(d, n, lmbd)
+            attn = LinearAttention(d, n, lmbd)
+            nn.init.xavier_normal_(attn.P, gain=0.1)
+            nn.init.xavier_normal_(attn.Q, gain=0.1)
+            self.attn = attn
         elif mode == 'sequential':
             self.layers = nn.ModuleList(
                 [LinearAttention(d, n, lmbd) for _ in range(l)])
+            for attn in self.layers:
+                nn.init.xavier_normal_(attn.P, gain=0.1/l)
+                nn.init.xavier_normal_(attn.Q, gain=0.1/l)
 
     def forward(self, Z):
         if self.mode == 'auto':
@@ -58,20 +64,21 @@ class LinearTransformer(nn.Module):
         else:
             for attn in self.layers:
                 Z = attn(Z)
-        
+
         return Z
 
 
 if __name__ == '__main__':
     from experiment.prompt import Prompt
-    d = 4
-    n = 10
-    l = 5
+    d = 3
+    n = 6
+    l = 10
+    torch.random.manual_seed(0)
     gamma = 0.9
     lmbd = 0.9
     prompt = Prompt(d, n, gamma)
     Z_0 = prompt.z()
-    ltf = LinearTransformer(d, n, l, lmbd, mode='auto')
-    _, Z_tf = ltf(Z_0)
+    ltf = LinearTransformer(d, n, l, lmbd, mode='sequential')
+    Z_tf = ltf(Z_0)
     print(Z_tf)
     print(Z_tf.shape)
