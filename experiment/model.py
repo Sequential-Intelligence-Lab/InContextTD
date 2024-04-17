b889ae230ba7a0bb67a094ac139b177566eb9b51
Jiuqi Wang <leonardowang597@gmail.com>
2024-04-17 01:19:35 -0400

    1. merge manual and direct value prediction 2. compute the TF fitted value function from context 3. small refactor on manual weight extraction

--- experiment/model.py
+++ experiment/model.py
@@ -56,6 +56,9 @@ class LinearTransformer(nn.Module):
                 nn.init.xavier_normal_(attn.Q, gain=0.1/l)
 
     def forward(self, Z):
+        '''
+        Z: prompt of shape (2*d+1, n+1)
+        '''
         if self.mode == 'auto':
             for _ in range(self.l):
                 Z = self.attn(Z)
@@ -64,73 +67,80 @@ class LinearTransformer(nn.Module):
                 Z = attn(Z)
 
         return Z
-    
+
     def manual_weight_extraction(self,
-                             Z: torch.Tensor,
-                             d: int):
+                                 context: torch.Tensor,
+                                 d: int):
         '''
-        tf: transformer model
-        Z: prompt
+        context: the context of shape (2*d+1, n)
         d: feature dimension
         '''
-
-        context = Z[:, :-1]
         weight = []
         for i in range(d):
-            query = torch.zeros((2*d+1, 1))
-            query[i, 0] = -1
-            Z_p = torch.concat([context, query], dim=1)
+            query_col = torch.zeros((2*d+1, 1))
+            query_col[i, 0] = -1
+            Z_p = torch.concat([context, query_col], dim=1)
             Z_tf = self.forward(Z_p)
             weight.append(Z_tf[-1, -1])
         weight = torch.stack(weight, dim=0)
         return weight.reshape((d, 1))
-    
-    def pred_v_manual(
-            self,
-            context: torch.tensor,
-            X: np.ndarray) -> torch.tensor:
-        d = X.shape[1]
-        X = torch.from_numpy(X)
-        w_tf = self.manual_weight_extraction(context, d)
-        v_vec = w_tf.t() @ X.T
-        return v_vec
-    
-    # Computes the value function for all the features in X given the context using the forward pass
-    # and taking the negative of the bottom right element of the output tensor
-    # returns a tensor of shape (len(X), 1)
-    def pred_v_array(self,
-              context: torch.tensor,
-              X: np.ndarray) -> torch.tensor:
-        d = X.shape[1]
-        X = torch.from_numpy(X)
-        tf_v = []
-        for feature in X:
-            feature_col = torch.zeros((2*d+1, 1))
-            feature_col[:d, 0] = feature
-            Z_p = torch.cat([context, feature_col], dim=1)
-            Z_tf = self.forward(Z_p)
-            tf_v.append(-Z_tf[-1, -1])
-        tf_v = torch.stack(tf_v, dim=0).reshape(-1, 1)
-        return tf_v
-    
-    # Computes the estimated value function based on the query
-    def pred_v(self, Z: torch.Tensor):
-        return -self.forward(Z)[-1, -1]
-    
 
+
+    def fit_value_func(self,
+                       context: torch.tensor,
+                       X: torch.tensor,
+                       manual: bool = False) -> torch.tensor:
+        '''
+        context: the context of shape (2*d+1, n)
+        X: features of shape (s, d)
+        manual: whether to use manual weight extraction or not
+        returns the fitted value function given the context in shape (s, 1)
+        '''
+        if manual:
+            w_tf = self.manual_weight_extraction(context, self.d)
+            return X @ w_tf
+        else:
+            tf_v = []
+            for feature in X:
+                feature_col = torch.zeros((2*d+1, 1))
+                feature_col[:d, 0] = feature
+                Z_p = torch.cat([context, feature_col], dim=1)
+                v_tf = self.pred_v(Z_p)
+                tf_v.append(v_tf)
+            tf_v = torch.stack(tf_v, dim=0).unsqueeze(1)
+            return tf_v
+
+    def pred_v(self, Z: torch.Tensor, manual: bool = False):
+        '''
+        Z: prompt of shape (2*d+1, n+1)
+        manual: whether to use manual weight extraction or not
+        predict the value of the query feature
+        '''
+        if manual:
+            context = Z[:, :-1]
+            query = Z[:self.d, [-1]]
+            w_tf = self.manual_weight_extraction(context, self.d)
+            return w_tf.t() @ query
+        else:
+            Z_tf = self.forward(Z)
+            return -Z_tf[-1, -1]
 
 
 if __name__ == '__main__':
-    from experiment.prompt import Prompt
-    d = 3
+    from experiment.prompt import MDPPromptGenerator
+    d = 2
+    s = 10
     n = 6
-    l = 10
+    l = 1
     torch.random.manual_seed(0)
+    np.random.seed(0)
     gamma = 0.9
-    lmbd = 0.9
-    prompt = Prompt(d, n, gamma)
-    Z_0 = prompt.z()
-    ltf = LinearTransformer(d, n, l, lmbd, mode='sequential')
-    Z_tf = ltf(Z_0)
-    print(Z_tf)
-    print(Z_tf.shape)
+    lmbd = 0.0
+    prompt_gen = MDPPromptGenerator(s, d, n, gamma)
+    prompt_gen.reset_feat()
+    prompt_gen.reset_mdp(sample_weight=False)
+    mdp_prompt = prompt_gen.get_prompt()
+    Z_0 = mdp_prompt.reset()
+    ltf = LinearTransformer(d, n, l, lmbd, mode='auto')
+    v_func = ltf.fit_value_func(mdp_prompt.context(), mdp_prompt.get_feature_mat(), manual=False)
+    print(v_func.shape)
