5c66ca4f5383b9090993e342db064c1f9bcb7941
Jiuqi Wang <43143815+LeonardoWjq@users.noreply.github.com>
2024-04-20 09:49:59 -0400

    Merging jiuqi-dev into master (#7)
    * refactor the training method
    save data in npz format
    support matrix  printing for sequential
    
    * rename data file

--- experiment/discounted_train.py
+++ experiment/discounted_train.py
@@ -3,7 +3,6 @@ import json
 import os
 import pickle
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.optim as optim
@@ -15,7 +14,7 @@ from experiment.utils import (compute_msve, solve_mspbe_weight,
                               solve_msve_weight, set_seed)
 
 from experiment.plotter import plot_data, plot_multiple_runs, print_final_weights
-
+from tqdm import tqdm
 
 def compute_tf_msve(v_tf: np.ndarray,
                     v_true: np.ndarray,
@@ -38,6 +37,35 @@ def compute_tf_mspbe(v_tf: np.ndarray,
     pbe = projection @ (r + gamma * P @ v_tf - v_tf)
     mspbe = steady_dist.dot(pbe**2)
     return mspbe.item()
+
+def _init_log() -> dict:
+    log = {'xs': [],
+           'mstde': [],
+           'msve weight error norm': [],
+           'mspbe weight error norm': [],
+           'true msve': [],
+           'transformer msve': [],
+           'transformer mspbe': [],
+           'P': [],
+           'Q': []
+           }
+    return log
+
+def _init_save_dir(save_dir: str) -> None:
+    if save_dir is None:
+        startTime = datetime.datetime.now()
+        save_dir = os.path.join('./logs', 
+                                "discounted_train", 
+                                startTime.strftime("%Y-%m-%d-%H-%M-%S"))
+        
+    # Create directory if it doesn't exist
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+def _save_log(log: dict, save_dir: str) -> None:
+    for key, value in log.items():
+        log[key] = np.array(value)
+    np.savez(os.path.join(save_dir, 'data.npz'), **log)
 
 
 def train(d: int,
@@ -74,29 +102,20 @@ def train(d: int,
     mini_batch_size: mini batch size
     '''
 
-    if save_dir is None:
-        startTime = datetime.datetime.now()
-        save_dir = os.path.join(
-            './logs', "discounted_train", startTime.strftime("%Y-%m-%d-%H-%M-%S"))
+    _init_save_dir(save_dir)
 
     set_seed(random_seed)
-    tf = LinearTransformer(d, n, l, lmbd, mode='auto')
+
+    tf = LinearTransformer(d, n, l, lmbd, mode=mode)
+
     opt = optim.Adam(tf.parameters(), lr=lr, weight_decay=weight_decay)
 
-    log = {'xs': [],
-           'mstde': [],
-           'msve weight error norm': [],
-           'mspbe weight error norm': [],
-           'true msve': [],
-           'transformer msve': [],
-           'transformer mspbe': [],
-            'P': [],
-            'Q': []
-           }
+    log = _init_log()
 
     pro_gen = MDPPromptGenerator(s, d, n, gamma)
+
     ### Training Loop ###
-    for i in range(n_mdps):
+    for i in tqdm(range(1, n_mdps+1)):
         pro_gen.reset_feat()  # reset feature
         pro_gen.reset_mdp(sample_weight=sample_weight)  # reset MDP
         prompt = pro_gen.get_prompt()  # get prompt object
@@ -119,10 +138,8 @@ def train(d: int,
             prompt.reset()  # reset prompt for fair testing
             mdp = prompt.mdp
             phi = prompt.feature_fun.phi
-            w_tf = tf.manual_weight_extraction(
-                prompt.context(), d).detach().numpy()
-            v_tf = tf.fit_value_func(
-                prompt.context(), torch.from_numpy(phi)).detach().numpy()
+            w_tf = tf.manual_weight_extraction(prompt.context(), d).detach().numpy()
+            v_tf = tf.fit_value_func(prompt.context(), torch.from_numpy(phi)).detach().numpy()
 
             log['xs'].append(i)
             log['mstde'].append(mstde.item())
@@ -145,63 +162,22 @@ def train(d: int,
                 v_tf, phi, mdp.P, mdp.r, gamma, mdp.steady_d)
             log['transformer mspbe'].append(tf_mspbe)
 
-            log['P'].append(tf.attn.P.detach().numpy().copy())
-            log['Q'].append(tf.attn.Q.detach().numpy().copy())
+            if mode=='auto':
+                log['P'].append(np.expand_dims(tf.attn.P.detach().numpy(), axis=0))
+                log['Q'].append(np.expand_dims(tf.attn.Q.detach().numpy(), axis=0))
+            else:
+                log['P'].append(np.stack([layer.P.detach().numpy() for layer in tf.layers]))
+                log['Q'].append(np.stack([layer.Q.detach().numpy() for layer in tf.layers]))
 
-            print('Step:', i)
-            #print('Transformer Learned Weight:\n', w_tf)
-            #print('MSVE Weight:\n', w_msve)
-            #print('MSPBE Weight:\n', w_mspbe)
-
-    prompt.reset()  # reset prompt for fair testing
-    mdp = prompt.mdp
-    phi = prompt.feature_fun.phi
-    w_tf = tf.manual_weight_extraction(prompt.context(), d).detach().numpy()
-    v_tf = tf.fit_value_func(
-        prompt.context(), torch.from_numpy(phi)).detach().numpy()
-
-    log['xs'].append(n_mdps)
-    log['mstde'].append(mstde.item())
-
-    w_msve = solve_msve_weight(mdp.steady_d, phi, mdp.v)
-    log['msve weight error norm'].append(
-        weight_error_norm(w_tf, w_msve).item())
-
-    w_mspbe = solve_mspbe_weight(mdp.steady_d, mdp.P, phi, mdp.r, gamma)
-    log['mspbe weight error norm'].append(
-        weight_error_norm(w_tf, w_mspbe).item())
-
-    true_msve = compute_msve(w_msve, mdp.steady_d, phi, mdp.v)
-    log['true msve'].append(true_msve)
-    tf_msve = compute_tf_msve(v_tf, mdp.v, mdp.steady_d)
-    log['transformer msve'].append(tf_msve)
-
-    tf_mspbe = compute_tf_mspbe(v_tf, phi, mdp.P, mdp.r, gamma, mdp.steady_d)
-    log['transformer mspbe'].append(tf_mspbe)
-
-    log['P'].append(tf.attn.P.detach().numpy().copy())
-    log['Q'].append(tf.attn.Q.detach().numpy().copy())
-
-    print('Step:', n_mdps)
-    print('Transformer Learned Weight:\n', w_tf)
-    print('MSVE Weight:\n', w_msve)
-    print('MSPBE Weight:\n', w_mspbe)
-
-    # Create directory if it doesn't exist
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    # Save log dictionary as JSON
-    with open(os.path.join(save_dir, 'discounted_train.pkl'), 'wb') as f:
-        pickle.dump(log, f)
+    _save_log(log, save_dir)
 
     hyperparameters = {
         'd': d,
         's': s,
         'n': n,
         'l': l,
-        'gamma': gamma,
         'lmbd': lmbd,
+        'gamma': gamma,
         'sample_weight': sample_weight,
         'manual': manual,
         'n_mdps': n_mdps,
@@ -217,84 +193,8 @@ def train(d: int,
     with open(os.path.join(save_dir, 'params.json'), 'w') as f:
         json.dump(hyperparameters, f)
 
-    plot_data(log, save_dir)
-    evaluate_weights(tf, save_dir)
-
-
-def plot_data(log, save_dir):
-
-    # Loss Plot
-    plt.figure()
-    plt.plot(log['xs'], log['mstde'], label='MSTDE')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.title('Loss vs Epochs')
-    plt.legend()
-    plt.savefig(os.path.join(save_dir, 'loss_mstde.png'), dpi=300)
-
-    # Weight norm plot
-    plt.figure()
-    plt.plot(log['xs'], log['msve weight error norm'],
-             label='MSVE Weight Error Norm')
-    plt.plot(log['xs'], log['mspbe weight error norm'],
-             label='MSPBE Weight Error Norm')
-    plt.xlabel('Epochs')
-    plt.ylabel('Weight Error L2 Norm')
-    plt.title('Weight Error Norm vs Epochs')
-    plt.legend()
-    plt.savefig(os.path.join(save_dir, 'weight_error_norm.png'), dpi=300)
-
-    # Value Error Plot
-    plt.figure()
-    plt.plot(log['xs'], log['true msve'], label='True MSVE')
-    plt.plot(log['xs'], log['transformer msve'], label='Transformer MSVE')
-    plt.xlabel('Epochs')
-    plt.ylabel('MSVE')
-    plt.title('MSVE vs Epochs')
-    plt.legend()
-    plt.savefig(os.path.join(save_dir, 'msve.png'), dpi=300)
-
-    # MSPBE Plot
-    plt.figure()
-    plt.plot(log['xs'], log['transformer mspbe'], label='Transformer MSPBE')
-    plt.xlabel('Epochs')
-    plt.ylabel('MSPBE')
-    plt.title('MSPBE vs Epochs')
-    plt.legend()
-    plt.savefig(os.path.join(save_dir, 'mspbe.png'), dpi=300)
-
-
-def evaluate_weights(tf, save_dir):
-    if tf.mode == 'auto':
-        # Save the final P and Q matrices
-        final_P = tf.attn.P.detach().numpy()
-        final_P /= np.max(np.abs(final_P))
-        final_Q = tf.attn.Q.detach().numpy()
-        final_Q /= np.max(np.abs(final_Q))
-
-        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5), sharey=True)
-        cax1 = axs[0].matshow(final_P, vmin=-1, vmax=1, cmap='gray')
-        axs[0].set_title('Final P Matrix')
-        axs[1].matshow(final_Q, vmin=-1, vmax=1, cmap='gray')
-        axs[1].set_title('Final Q Matrix')
-        fig.colorbar(cax1, ax=axs, orientation='vertical')
-        plt.savefig(os.path.join(save_dir, 'final_PQ.png'), dpi=300)
-        plt.close(fig)
-    else: # sequential
-        for l, layer in enumerate(tf.layers):
-            final_P = layer.P.detach().numpy()
-            final_P /= np.max(np.abs(final_P))
-            final_Q = layer.Q.detach().numpy()
-            final_Q /= np.max(np.abs(final_Q))
-
-            fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5), sharey=True)
-            cax1 = axs[0].matshow(final_P, vmin=-1, vmax=1, cmap='gray')
-            axs[0].set_title(f'Final P Matrix Layer {l+1}')
-            axs[1].matshow(final_Q, vmin=-1, vmax=1, cmap='gray')
-            axs[1].set_title(f'Final Q Matrix Layer {l+1}')
-            fig.colorbar(cax1, ax=axs, orientation='vertical')
-            plt.savefig(os.path.join(save_dir, f'final_PQ_{l+1}.png'), dpi=300)
-            plt.close(fig)
+    # plot_data(log, save_dir)
+    print_final_weights(tf, save_dir)
 
 
 def run_hyperparam_search():
@@ -311,18 +211,18 @@ def run_hyperparam_search():
             train(d, s, n, l, lmbd=0.0, sample_weight=sw, epochs=25_000,
                   log_interval=250, save_dir='l{layer}_s{s_}_sw{samp_w}'.format(layer=l, s_=s, samp_w=sw))
 
-    print_final_weights(tf, save_dir)
-
 if __name__ == '__main__':
     d = 4
     n = 100
     l = 4
     s = int(n/10)
     startTime = datetime.datetime.now()
-    save_dir = os.path.join(
-            './logs', "discounted_train", startTime.strftime("%Y-%m-%d-%H-%M-%S"))
-    for seed in [1, 2, 3, 42, 456]:
-        train(d, s, n, l, lmbd=0.0,  n_mdps=5000, log_interval=25, random_seed=seed, save_dir= os.path.join(save_dir, f'seed_{seed}'))
+    save_dir = os.path.join('./logs', "discounted_train", startTime.strftime("%Y-%m-%d-%H-%M-%S"))
+    for seed in [1, 2, 3]:
+        train(d, s, n, l, lmbd=0.0,  n_mdps=60, log_interval=25, random_seed=seed, save_dir= os.path.join(save_dir, f'seed_{seed}'), mode='sequential')
+    # with np.load('./logs/discounted_train/2024-04-19-20-09-57/seed_1/discounted_train.npz') as data:
+    #     for key, value in data.items():
+    #         print(key, value.shape)
     
 
 
