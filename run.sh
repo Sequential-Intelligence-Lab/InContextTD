7677a06519a67c8c0fbbe3a35b1aea3bbccbdd36
Ethan Blaser <ethanblaser1@gmail.com>
2024-10-03 10:47:42 -0400

    update run.sh to parallelize

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 ####################################################### linear transformer ####################################################### 
-python main.py --suffix=standard --linear -v # standard
+python main.py --suffix=standard --linear -v & # standard
 sleep 1
-python main.py --suffix=2layers -l=2 --linear -v # 2 layers auto
+python main.py --suffix=2layers -l=2 --linear -v & # 2 layers auto
 sleep 1
-python main.py --suffix=4layers -l=4 --linear -v # 4 layers auto
+python main.py --suffix=4layers -l=4 --linear -v & # 4 layers auto
 sleep 1
-python main.py --suffix=sequential --mode=sequential --linear -v # standard sequential
+python main.py --suffix=sequential --mode=sequential --linear -v & # standard sequential
 sleep 1
-python main.py --suffix=sequential_2layers -l=2 --mode=sequential --linear -v # 2 layer sequential
+python main.py --suffix=sequential_2layers -l=2 --mode=sequential --linear -v &  # 2 layer sequential
 sleep 1
-python main.py --suffix=sequential_4layers -l=4 --mode=sequential --linear -v # 4 layer sequential
+python main.py --suffix=sequential_4layers -l=4 --mode=sequential --linear -v & # 4 layer sequential
 sleep 1
-python main.py --suffix=large -d=8 -s=20 -n=60 --linear -v # larger scale
+python main.py --suffix=large -d=8 -s=20 -n=60 --linear -v & # larger scale
 sleep 1
 wait
 
