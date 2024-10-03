96a6dd61899148d808e112f12076dfb3a6e247a0
Jiuqi Wang <leonardowang597@gmail.com>
2024-10-02 20:09:51 -0400

    add average reward

--- theory_verification/model.py
+++ theory_verification/model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 from experiment.utils import stack_four
 
+# TD layer
+
 
 class DiscoutedTDLayer(nn.Module):
     def __init__(self, d: int, n: int, lmbd: float):
@@ -30,7 +32,7 @@ class DiscoutedTDLayer(nn.Module):
         self.Q = torch.zeros_like(self.P)
         self.Q[:2*d, :2*d] = self.A
 
-    def forward(self, Z: torch.Tensor, alpha: float = 1.0):
+    def forward(self, Z: torch.Tensor, alpha: float = 1.0) -> torch.Tensor:
         '''
         Z: prompt matrix
         alpha: learning rate
@@ -49,8 +51,10 @@ class DiscountedTDTransformer(nn.Module):
         '''
         super(DiscountedTDTransformer, self).__init__()
         self.n = n
-        self.layers = nn.ModuleList([DiscoutedTDLayer(d, n, lmbd) for _ in range(l)])
-        self.Cs = [layer.C for layer in self.layers] # preconditioning matrices
+        self.layers = nn.ModuleList(
+            [DiscoutedTDLayer(d, n, lmbd) for _ in range(l)])
+        # preconditioning matrices
+        self.Cs = [layer.C for layer in self.layers]
 
     def forward(self, Z: torch.Tensor, alpha: float = 1.0) -> torch.Tensor:
         '''
@@ -60,10 +64,11 @@ class DiscountedTDTransformer(nn.Module):
         v = []
         for layer in self.layers:
             Z = layer.forward(Z, alpha)
-            # negate it to align with the convention
             v.append(-Z[-1, -1].item())
-        v = torch.tensor(v) 
+        v = torch.tensor(v)
         return v
+
+# residual gradient layer
 
 
 class RGLayer(nn.Module):
@@ -83,16 +88,17 @@ class RGLayer(nn.Module):
         O = torch.zeros((d, d))
         self.M1 = stack_four(-I, I, O, O)
         self.M2 = -self.M1
-        self.C = torch.eye(d)  # torch.randn(d, d)
+        self.C = torch.eye(d)
         self.B = stack_four(self.C.t(), O, O, O)
         self.A = self.M2.t() @ self.B @ self.M1
         self.Q = torch.zeros_like(self.P)
         self.Q[:2*d, :2*d] = self.A
 
-    def forward(self, Z: torch.Tensor, alpha: float = 1.0):
+    def forward(self, Z: torch.Tensor, alpha: float = 1.0) -> torch.Tensor:
         next_Z = Z + alpha / self.n * self.P @ Z @ self.M @ Z.T @ self.Q @ Z
         return next_Z
-    
+
+
 class RGTransformer(nn.Module):
     def __init__(self, l: int, d: int, n: int):
         '''
@@ -102,8 +108,9 @@ class RGTransformer(nn.Module):
         '''
         super(RGTransformer, self).__init__()
         self.n = n
-        self.layers = nn.ModuleList([RGLayer(d, n)  for _ in range(l)])
-        self.Cs = [layer.C for layer in self.layers] # preconditioning matrices
+        self.layers = nn.ModuleList([RGLayer(d, n) for _ in range(l)])
+        # preconditioning matrices
+        self.Cs = [layer.C for layer in self.layers]
 
     def forward(self, Z: torch.Tensor, alpha: float = 1.0) -> torch.Tensor:
         '''
@@ -115,5 +122,68 @@ class RGTransformer(nn.Module):
             Z = layer.forward(Z, alpha)
             # negate it to align with the convention
             v.append(-Z[-1, -1].item())
-        v = torch.tensor(v) 
+        v = torch.tensor(v)
+        return v
+
+# averate reward TD layer
+
+
+class AVGREWTDLayer(nn.Module):
+    def __init__(self, d: int, n: int):
+        '''
+        d: feature dimension
+        n: context length
+        '''
+        super(AVGREWTDLayer, self).__init__()
+        self.d = d
+        self.n = n
+
+        self.P1 = torch.zeros((2 * d + 2, 2 * d + 2))
+        self.P1[-2, -2] = 1  # head 1, filter out the reward row only
+        self.P2 = torch.zeros((2 * d + 2, 2 * d + 2))
+        # head 2, filter out the (cumulative) reward bar row only
+        self.P2[-1, -1] = 1
+
+        s = torch.ones((n + 1, n + 1))
+        s = torch.triu(s)
+        diag = torch.diag(torch.tensor([1/k for k in range(1, n + 2)]))
+        self.R = s @ diag - torch.eye(n + 1)  # for computing r bar - r
+
+        I = torch.eye(d)
+        O = torch.zeros((d, d))
+        self.M = torch.eye(n + 1)
+        self.M[-1, -1] = 0
+        self.M1 = stack_four(-I, I, O, O)
+        self.C = torch.randn(d, d)
+        self.B = stack_four(self.C.t(), O, O, O)
+        self.A = torch.mm(self.B, self.M1)
+        self.Q = torch.zeros_like(self.P1)
+        self.Q[:2*d, :2*d] = self.A
+
+        self.W = torch.zeros((2*d+2, 4*d + 4))
+        self.W[-1, 2*d] = 1
+        self.W[-1, -1] = 1
+
+    def forward(self, Z: torch.Tensor, alpha: float = 1.0) -> torch.Tensor:
+        head1 = self.P1 @ Z @ self.R @ self.M @ Z.T @ self.Q @ Z
+        head2 = self.P2 @ Z @ self.M @ Z.T @ self.Q @ Z
+        multihead = torch.concat([head1, head2], dim=0)
+
+        next_Z = Z + alpha / self.n * self.W @ multihead
+        return next_Z
+
+
+class AVGREWTDTransformer(nn.Module):
+    def __init__(self, l: int, d: int, n: int):
+        super(AVGREWTDTransformer, self).__init__()
+        self.n = n
+        self.layers = nn.ModuleList([AVGREWTDLayer(d, n) for _ in range(l)])
+        self.Cs = [layer.C for layer in self.layers]
+
+    def forward(self, Z: torch.Tensor, alpha: float = 1.0) -> torch.Tensor:
+        v = []
+        for layer in self.layers:
+            Z = layer.forward(Z, alpha)
+            v.append(Z[-1, -1].item())
+        v = torch.tensor(v)
         return v
--- theory_verification/verify.py
+++ theory_verification/verify.py
@@ -3,7 +3,7 @@ from typing import Dict
 import numpy as np
 import torch
 
-from theory_verification.model import DiscountedTDTransformer, RGTransformer
+from theory_verification.model import DiscountedTDTransformer, RGTransformer, AVGREWTDTransformer
 from theory_verification.prompt import Prompt
 
 
@@ -14,38 +14,49 @@ def verify(d: int, n: int, l: int) -> np.ndarray:
     l: number of layers (updates)
     '''
     pro_discounted = Prompt(d, n, 0.9)
+    pro_avg = Prompt(d, n, 1.0)
 
     td0_tf = DiscountedTDTransformer(l, d, n, 0.0)
     tdlmbd_tf = DiscountedTDTransformer(l, d, n, 0.5)
     rg_tf = RGTransformer(l, d, n)
+    avg_rew_td_tf = AVGREWTDTransformer(l, d, n)
     td0_tf_value = td0_tf(pro_discounted.z()).numpy()
     tdlmbd_tf_value = tdlmbd_tf(pro_discounted.z()).numpy()
     rg_tf_value = rg_tf(pro_discounted.z()).numpy()
+    avg_rew_td_tf_value = avg_rew_td_tf(pro_avg.z_avg_rew()).numpy()
 
     w_td0 = torch.zeros((d, 1))
     w_tdlmbd = torch.zeros((d, 1))
     w_rg = torch.zeros((d, 1))
+    w_avg_rew_td = torch.zeros((d, 1))
     td0_value = []
     tdlmbd_value = []
     rg_value = []
+    avg_rew_rd_value = []
     for i in range(l):
         w_td0, v_td0 = pro_discounted.td_update(w_td0, td0_tf.Cs[i], 0.0)
         td0_value.append(v_td0)
 
-        w_tdlmbd, v_tdlmbd = pro_discounted.td_update(w_tdlmbd, 
+        w_tdlmbd, v_tdlmbd = pro_discounted.td_update(w_tdlmbd,
                                                       tdlmbd_tf.Cs[i], 0.5)
         tdlmbd_value.append(v_tdlmbd)
 
         w_rg, v_rg = pro_discounted.rg_update(w_rg, rg_tf.Cs[i])
         rg_value.append(v_rg)
 
+        w_avg_rew_td, v_avg_rew_td = pro_avg.avg_rew_td_update(w_avg_rew_td,
+                                                               avg_rew_td_tf.Cs[i])
+        avg_rew_rd_value.append(v_avg_rew_td)
+
     td0_value = np.array(td0_value)
     tdlmbd_value = np.array(tdlmbd_value)
     rg_value = np.array(rg_value)
+    avg_rew_rd_value = np.array(avg_rew_rd_value)
 
     return dict(td0=np.absolute(td0_tf_value - td0_value),
                 tdlambda=np.absolute(tdlmbd_tf_value - tdlmbd_value),
-                rg=np.absolute(rg_tf_value - rg_value))
+                rg=np.absolute(rg_tf_value - rg_value),
+                avg_rew_td=np.absolute(avg_rew_td_tf_value - avg_rew_rd_value))
 
 
 if __name__ == '__main__':
