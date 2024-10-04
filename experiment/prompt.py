5d48fd241cc41828f38c679f22d47a5f43f1930a
Jiuqi Wang <leonardowang597@gmail.com>
2024-10-03 22:13:18 -0400

    change MDP to mrp simplify td update

--- experiment/prompt.py
+++ experiment/prompt.py
@@ -5,25 +5,26 @@ import torch
 
 from MRP.boyan import BoyanChain
 from MRP.loop import Loop
-
+from MRP.mrp import MRP
+from typing import Tuple
 
 class Feature:
     def __init__(self, d: int, s: int, mode: str = 'random'):
         '''
         d: dimension of the feature vector
         s: number of states
-
+        mode: feature type
         '''
         self.d = d
         self.s = s
         if mode == 'random':
-            self.phi = np.random.uniform(low=-1, high=1, size=(s, d)).astype(np.float32)
+            self.phi = np.random.uniform(low=-1, high=1,
+                                         size=(s, d)).astype(np.float32)
         elif mode == 'one-hot':
             assert s == d, "number of states must be equal to the feature dimension"
             self.phi = np.eye(s, dtype=np.float32)
         else:
             raise ValueError("Unknown mode")
-            
 
     def __call__(self, s: int):
         return self.phi[s]
@@ -34,34 +35,34 @@ class Feature:
         return f
 
 
-class MDPPrompt:
+class MRPPrompt:
     def __init__(self,
                  d: int,
                  n: int,
                  gamma: float,
-                 mdp: BoyanChain,
+                 mrp: MRP,
                  feature_fun: Feature):
         '''
         d: feature dimension
         n: context length
         gamma: discount factor
-        mdp: an instance of a BoyanChain MDP
+        mrp: an instance of a Markov Reward Process
         feature_fun: a function that returns the feature vector of a state  
         '''
         self.d = d
         self.n = n
         self.gamma = gamma
-        self.mdp = mdp
+        self.mrp = mrp
         self.feature_fun = feature_fun
 
-    def reset(self):
+    def reset(self) -> torch.Tensor:
         self.feature_window = deque(maxlen=self.n+2)
         self.reward_window = deque(maxlen=self.n+1)
         # populates the feature and rewards
-        self.s = self.mdp.reset()
+        self.s = self.mrp.reset()
         self.feature_window.append(self.feature_fun(self.s))
         for _ in range(self.n+1):
-            s_prime, r = self.mdp.step(self.s)
+            s_prime, r = self.mrp.step(self.s)
             self.feature_window.append(self.feature_fun(s_prime))
             self.reward_window.append(r)
             self.s = s_prime
@@ -70,9 +71,9 @@ class MDPPrompt:
 
         return self.z()
 
-    def step(self):
-        # step the MDP
-        s_prime, r = self.mdp.step(self.s)
+    def step(self) -> Tuple[torch.Tensor, float]:
+        # step the MRP
+        s_prime, r = self.mrp.step(self.s)
         self.feature_window.append(self.feature_fun(s_prime))
         self.reward_window.append(r)
         self.s = s_prime
@@ -89,10 +90,10 @@ class MDPPrompt:
         self._context = torch.concat([self.phi, self.phi_prime, self.r], dim=0)
         self._query = torch.from_numpy(features[self.n+1]).reshape(self.d, 1)
 
-    def context(self):
+    def context(self) -> torch.Tensor:
         return self._context
 
-    def query(self):
+    def query(self) -> torch.Tensor:
         return self._query
 
     def set_query(self, query: torch.Tensor):
@@ -105,93 +106,87 @@ class MDPPrompt:
     def disable_query_grad(self):
         self._query.requires_grad_(False)
 
-    def query_grad(self):
+    def query_grad(self) -> torch.Tensor:
         assert self._query.grad is not None, "no gradient associated with the query"
         return self._query.grad.reshape((self.d, 1))
 
     def zero_query_grad(self):
         self._query.grad = None
 
-    def get_feature_mat(self):
+    def get_feature_mat(self) -> torch.Tensor:
         return torch.from_numpy(self.feature_fun.phi)
 
-    def z(self):
-        query_col = torch.concat(
-            [self._query, torch.zeros((self.d+1, 1))], dim=0)
+    def z(self) -> torch.Tensor:
+        query_col = torch.concat([self._query, torch.zeros((self.d+1, 1))],
+                                 dim=0)
         return torch.concat([self._context, query_col], dim=1)
 
     def td_update(self,
                   w: torch.Tensor,
-                  C: torch.Tensor = None,
-                  residual: bool = False,
-                  lr: float = 1):
+                  lr: float = 1.0) -> Tuple[torch.Tensor, float]:
         '''
         w: weight vector
-        C: preconditioning matrix
+        lr: learning rate
         '''
-        u = 0
-        for j in range(self.n):
-            target = self.r[0, j] + w.t() @ self.phi_prime[:, [j]]
-            td_error = target - w.t() @ self.phi[:, [j]]
-            if residual:
-                u += td_error * (self.phi[:, [j]] - self.phi_prime[:, [j]])
-            else:
-                u += td_error * self.phi[:, [j]]
-        u *= lr/self.n
-        if C:
-            u = C @ u  # apply conditioning matrix
-        new_w = w + u
-        v = new_w.t() @ self.phi[:, [-1]]
-        return new_w, v.item()
+        u = torch.zeros((self.d, 1))
+        for i in range(self.n):
+            target = self.r[0, i] + w.t() @ self.phi_prime[:, [i]]
+            tde = target - w.t() @ self.phi[:, [i]]
+            u += tde * self.phi[:, [i]]
 
-    def copy(self) -> 'MDPPrompt':
-        mdp_prompt = MDPPrompt(self.d, self.n, self.gamma,
-                               self.mdp.copy(), self.feature_fun.copy())
-        mdp_prompt.feature_window = self.feature_window.copy()
-        mdp_prompt.reward_window = self.reward_window.copy()
-        mdp_prompt.s = self.s
-        mdp_prompt._store_data()
-        return mdp_prompt
+        w += lr/self.n * u
+        v = w.t() @ self.phi[:, [-1]]
+        return w, v.item()
+
+    def copy(self) -> 'MRPPrompt':
+        mrp_prompt = MRPPrompt(self.d, self.n, self.gamma,
+                               self.mrp.copy(), self.feature_fun.copy())
+        mrp_prompt.feature_window = self.feature_window.copy()
+        mrp_prompt.reward_window = self.reward_window.copy()
+        mrp_prompt.s = self.s
+        mrp_prompt._store_data()
+        return mrp_prompt
 
 
-class MDPPromptGenerator:
+class MRPPromptGenerator:
     def __init__(self,
                  s: int,
                  d: int,
                  n: int,
                  gamma: float,
-                 mdp_class: str = 'boyan'):
+                 mrp_class: str = 'boyan'):
         '''
         s: number of states
         d: feature dimension
         n: context length
         gamma: discount factor
+        mrp_class: type of MRP
         '''
 
         self.s = s
         self.d = d
         self.n = n
         self.gamma = gamma
-        self.mdp_class = mdp_class
+        self.mrp_class = mrp_class
 
-    def reset_mdp(self, sample_weight: bool = False, threshold: float = 0.5):
+    def reset_mrp(self, sample_weight: bool = False, threshold: float = 0.5):
         w = np.random.randn(self.d, 1) if sample_weight else None
-        if self.mdp_class == 'boyan':
-            self.mdp = BoyanChain(n_states=self.s, gamma=self.gamma,
+        if self.mrp_class == 'boyan':
+            self.mrp = BoyanChain(n_states=self.s, gamma=self.gamma,
                                   weight=w, X=self.feat.phi)
-        elif self.mdp_class == 'loop':
-            self.mdp = Loop(n_states=self.s, gamma=self.gamma, threshold=threshold,
+        elif self.mrp_class == 'loop':
+            self.mrp = Loop(n_states=self.s, gamma=self.gamma, threshold=threshold,
                             weight=w, Phi=self.feat.phi)
         else:
-            raise ValueError("Unknown MDP type")
+            raise ValueError("Unknown MRP type")
 
     def reset_feat(self):
         self.feat = Feature(self.d, self.s)
 
-    def get_prompt(self):
-        assert self.mdp is not None, "call reset_mdp first"
+    def get_prompt(self) -> MRPPrompt:
+        assert self.mrp is not None, "call reset_mrp first"
         assert self.feat is not None, "call reset_feat first"
-        return MDPPrompt(self.d, self.n, self.gamma, self.mdp, self.feat)
+        return MRPPrompt(self.d, self.n, self.gamma, self.mrp, self.feat)
 
 
 if __name__ == '__main__':
@@ -201,22 +196,22 @@ if __name__ == '__main__':
     eval_len = 3
     gamma = 0.9
 
-    prompt_gen = MDPPromptGenerator(s, d, n, gamma)
+    prompt_gen = MRPPromptGenerator(s, d, n, gamma)
     prompt_gen.reset_feat()
-    prompt_gen.reset_mdp(sample_weight=False)
-    mdp_prompt = prompt_gen.get_prompt()
-    Z_0 = mdp_prompt.reset()
+    prompt_gen.reset_mrp(sample_weight=False)
+    mrp_prompt = prompt_gen.get_prompt()
+    Z_0 = mrp_prompt.reset()
     print(Z_0)
-    Z_1 = mdp_prompt.step()
+    Z_1 = mrp_prompt.step()
     print(Z_1)
 
     prompt_gen.reset_feat()
-    prompt_gen.reset_mdp(sample_weight=True)
-    mdp_prompt = prompt_gen.get_prompt()
-    Z_0 = mdp_prompt.reset()
+    prompt_gen.reset_mrp(sample_weight=True)
+    mrp_prompt = prompt_gen.get_prompt()
+    Z_0 = mrp_prompt.reset()
     print(Z_0)
-    Z_1 = mdp_prompt.step()
+    Z_1 = mrp_prompt.step()
     print(Z_1)
 
-    pro = mdp_prompt.copy()
-    print(pro.mdp.w)
+    pro = mrp_prompt.copy()
+    print(pro.mrp.w)
