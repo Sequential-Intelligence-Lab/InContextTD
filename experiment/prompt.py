d2b3d236ca82a9d93b2dde6ae6ecaed8d192a99b
Jiuqi Wang <leonardowang597@gmail.com>
2024-10-03 21:40:16 -0400

    remove random prompt

--- experiment/prompt.py
+++ experiment/prompt.py
@@ -34,61 +34,6 @@ class Feature:
         return f
 
 
-class Prompt:
-    def __init__(self,
-                 d: int,
-                 n: int,
-                 gamma: float,
-                 w: torch.Tensor = None,
-                 noise: float = 0.1):
-        '''
-        d: feature dimension
-        n: context length
-        gamma: discount factor
-        w: weight vector (optional)
-        noise: reward noise level
-        '''
-        self.d = d
-        self.n = n
-        self.gamma = gamma
-        self.phi = torch.randn(d, n+1)
-
-        phi_prime = torch.randn(d, n)
-        phi_prime = torch.concat([phi_prime, torch.zeros((d, 1))], dim=1)
-        self.phi_prime = gamma * phi_prime
-
-        if w:
-            self.w = w
-        else:
-            self.w = torch.randn((d, 1))
-
-        r = self.w.t() @ self.phi - self.w.t() @ self.phi_prime
-        r[0, -1] = 0
-        self.r = r
-
-    def z(self):
-        return torch.cat([self.phi, self.phi_prime, self.r], dim=0)
-
-    def td_update(self,
-                  w: torch.Tensor,
-                  C: torch.Tensor = None):
-        '''
-        w: weight vector
-        C: preconditioning matrix
-        '''
-        u = 0
-        for j in range(self.n):
-            target = self.r[0, j] + w.t() @ self.phi_prime[:, [j]]
-            td_error = target - w.t() @ self.phi[:, [j]]
-            u += td_error * self.phi[:, [j]]
-        u /= self.n
-        if C:
-            u = C @ u  # apply conditioning matrix
-        new_w = w + u
-        v = new_w.t() @ self.phi[:, [-1]]
-        return new_w, v.item()
-
-
 class MDPPrompt:
     def __init__(self,
                  d: int,
